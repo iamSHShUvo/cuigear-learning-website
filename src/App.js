c7d9c1d9a9cc55a77d690d97fdfab2d590dd33d8
iamSHShUvo <2027shuvo@gmail.com>
2021-10-05 23:44:43 +0600

    Cuigear Learning Website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
+import About from './components/About/About';
+import Services from './components/Services/Services';
+import Contact from './components/Contact/Contact';
+import NotFound from './components/NotFound/NotFound';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Header></Header>
+        <Switch>
+
+          {/* Home Route */}
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route  path="/home">
+            <Home></Home>
+          </Route>
+
+          {/* About Route */}
+          <Route path="/about">
+            <About></About>
+          </Route>
+
+          {/* Services Route */}
+           <Route path="/services">
+            <Services></Services>
+          </Route>
+          {/* Contact Route */}
+          <Route path="/contact">
+            <Contact></Contact>
+          </Route>
+
+          {/* 404 Route */}
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
+      <Footer></Footer>
+     
+      {/* <Home></Home> */}
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className="bg-success">
+            <div className="row d-flex">
+                
+           
+                <div className="col-lg-12 col-sm-12  mt-2 mb-2 text-center text-white">
+                    Copyright © 2021-22 | CUIGEAR | All rights reserved.
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
+
+/* <div className="social-icon">
+<i class="fab fa-facebook-square"></i>
+<i class="fab fa-instagram-square"></i>
+<i class="fab fa-linkedin-in"></i>
+</div> */
